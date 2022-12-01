6304ca0121c3bcdec2edd2cda0d7cefad2148bfc
REALPROMMS <m.makarov90@bk.ru>
2022-12-01 14:30:45 +0300

    done 003

---
+++ Лекции C-SHARP/lesson2/example_003/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1,12,31,4,15,18,17,18,9};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+	if (array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+
+	index = index + 1; // index++
+}
+// задача - нужно найти элемент массива, равный числу find
