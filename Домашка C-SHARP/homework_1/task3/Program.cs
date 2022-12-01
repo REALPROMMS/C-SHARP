fe9e768a3458211d8905264bba97e196fb744b09
REALPROMMS <m.makarov90@bk.ru>
2022-12-01 10:29:31 +0300

    complete task3

--- Домашка C-SHARP/homework_1/task3/Program.cs
+++ Домашка C-SHARP/homework_1/task3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Усложнил и сделал с циклом.
+
+Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+while (n % 2 != 0)
+{
+	Console.WriteLine($"Число {n} является нечетным!/ Загадайте другое.");
+	Console.Write("Введите любое число: ");
+	n = int.Parse(Console.ReadLine());
+} 
+if (n % 2 == 0)
+{
+	Console.WriteLine($"Это число является четным: {n}");
+}
+
+
+// Простой способ.
+
+/*Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+	Console.WriteLine($"Это число является четным: {n}");
+} 
+else
+{
+	Console.WriteLine($"Число {n} является нечетным!/ Загадайте другое.");
+}
+*/
