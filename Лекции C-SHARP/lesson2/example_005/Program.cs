1fb275c5562991da7e72c3331a266c85123cfea1
REALPROMMS <m.makarov90@bk.ru>
2022-12-01 15:09:03 +0300

    done 005

---
+++ Лекции C-SHARP/lesson2/example_005/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection) 
+{
+	int length = collection.Length;
+	int index = 0;
+	while (index < length)
+	{
+		collection[index] = new Random().Next(1, 10);
+		index++;
+	}
+}
+void PrintArray(int[] col) 
+{
+	int count = col.Length;
+	int position = 0;
+	while (position < count)
+	{
+		Console.WriteLine(col[position]);
+		position++;
+	}
+}
+
+int IndexOf(int[] collection, int find)
+{
+	int count = collection.Length;
+	int index = 0;
+	int position = 0; // (-1) ставим , что если такого элемента не будет он покажет -1
+	
+	while (index < count)
+	{
+		if(collection[index] == find)
+		{
+			position = index;
+			break; // прерывание команды, что бы нашло 1ю (4ку) в массиве и показала его индекс ,а не последнюю.
+		}
+		index++;
+	}
+	return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+// array[4] = 4; принудительно меняем эти индексы в массиве на 4.
+// array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4); // Если будем искать элемент которого нет (255),
+                             // то он выдаст все равно идекс 0, поэтому нужно поменять в position на -1
+Console.WriteLine(pos);
+
+
