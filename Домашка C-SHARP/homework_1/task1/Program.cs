0b4f87e31207ebaf49dc68f15c610f1f3e07cc33
REALPROMMS <m.makarov90@bk.ru>
2022-12-01 09:51:09 +0300

    complete task1

--- Домашка C-SHARP/homework_1/task1/Program.cs
+++ Домашка C-SHARP/homework_1/task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Первый вариант решения этой задачи!
+
+int numberA = new Random().Next (1, 10); 
+Console.WriteLine($"Первое число: {numberA}");
+int numberB = new Random().Next (1, 10);
+Console.WriteLine($"Второе число: {numberB}");
+if (numberA > numberB) {
+	Console.WriteLine($"Результат: {numberA} больше, чем число {numberB}");
+}
+else {
+	Console.WriteLine($"Результат: {numberB} больше, чем число {numberA}");
+}
+
+
+// Второй вариант решения этой задачи!
+
+/*
+Console.Write("Введите любое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе любое число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB) {
+	Console.WriteLine($"Результат: {numberA} больше, чем число {numberB}");
+}
+else {
+	Console.WriteLine($"Результат: {numberB} больше, чем число {numberA}");
+}
+*/
