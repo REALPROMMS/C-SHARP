a2a3292400b83b58b2d3a25601101d8ad414c83c
REALPROMMS <m.makarov90@bk.ru>
2022-12-01 10:56:26 +0300

    complete task4

--- Домашка C-SHARP/homework_1/task4/Program.cs
+++ Домашка C-SHARP/homework_1/task4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+int m = 1;
+int i = 2;
+for ( i = m; i <= n; i++)
+ if ( i % 2 == 0) {
+	Console.WriteLine(i);
+ }
+Console.WriteLine($"Это все четные числа из указанного числа: {n}");
