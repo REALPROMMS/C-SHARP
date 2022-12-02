b7f42cc576caa75cdb0d63437a82540ea1964c86
REALPROMMS <m.makarov90@bk.ru>
2022-12-02 10:32:42 +0300

    done task3

---
+++ Практика C-SHARP/seminars2/task3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+
+string? s = Console.ReadLine(); // знак вопроса ставится ,что бы убрать предупреждение (что мы можем принять null)
+
+Console.WriteLine(Convert.ToInt32(s[0].ToString())); // переводим символ в строку и из строки в число
