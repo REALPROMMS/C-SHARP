310ee3861da7ec91a600809f389eecd39428ae8a
REALPROMMS <m.makarov90@bk.ru>
2022-12-02 10:59:10 +0300

    done task5

---
+++ Практика C-SHARP/seminars2/task5/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = 161;
+Console.WriteLine(number);
+if (number % 7 == 0 && number % 23 == 0) {
+	Console.WriteLine($"да число {number} является кратным 7 и 23");
+}
+else {
+	Console.WriteLine($"нет {number} неявляется кратным 7 и 23");
+}
+
+/*
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0)
+{
+    if (n % 23 == 0)
+        Console.WriteLine("yes");
+    else
+        Console.WriteLine("no");
+}
+else
+    Console.WriteLine("no");
+*/
