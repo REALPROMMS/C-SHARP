dc93acb3ef250c0dfe3d20b340d13103e5f3fd76
REALPROMMS <m.makarov90@bk.ru>
2022-12-02 10:21:43 +0300

    done task1

--- Практика C-SHARP/seminars2/task1/Program.cs
+++ Практика C-SHARP/seminars2/task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int n = new Random().Next(10, 100); // [begin, end+1] [10, 99]
+Console.WriteLine($"Случайное число {n}");
+int n1 = n / 10; // кол-во десятков
+int n2 = n % 10; // кол-во единиц
+if (n1 > n2)
+	Console.WriteLine(n1);
+else
+	Console.WriteLine(n2);
