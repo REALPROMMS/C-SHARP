3f86428e48a6fed65cbcfb6985b9a9ce7fb7ba56
REALPROMMS <m.makarov90@bk.ru>
2022-12-05 10:35:22 +0300

    done task1

--- Домашка C-SHARP/homework_2/task1/Program.cs
+++ Домашка C-SHARP/homework_2/task1/Program.cs
@@ -4,3 +4,6 @@ int numberA = new Random().Next(100, 999);
 string n = numberA.ToString();
 Console.WriteLine(numberA);
 Console.WriteLine(n[1]);	
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
