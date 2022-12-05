2203b0cd243bcca2ede6116a1ecf8c14bbda3f2b
REALPROMMS <m.makarov90@bk.ru>
2022-12-05 11:41:14 +0300

    done task2

---
+++ Домашка C-SHARP/homework_2/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите любое число");
+int number = int.Parse(Console.ReadLine());
+while (number < 99 || number > 1000)
+ {
+	Console.WriteLine("Данное число неявляется трёхзначным! Загадайте другое.");
+	Console.WriteLine("Введите любое число: ");
+	number = int.Parse(Console.ReadLine());
+ }
+ if (number >= 100 && number <= 999)
+ {
+		Console.WriteLine(number % 10);
+ }
+
+// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+// НЕ ИСПОЛЬЗОВАТЬ НУМЕРАЦИЮ СТРОК
