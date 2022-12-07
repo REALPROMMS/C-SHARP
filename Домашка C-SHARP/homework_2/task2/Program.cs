de6b988352421dd7699b8a3b3a54e65d3db59610
REALPROMMS <m.makarov90@bk.ru>
2022-12-07 18:28:14 +0300

    done task2

--- Домашка C-SHARP/homework_2/task2/Program.cs
+++ Домашка C-SHARP/homework_2/task2/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-Console.WriteLine("Введите любое число");
+/* Console.WriteLine("Введите любое число");
 int number = int.Parse(Console.ReadLine());
 while (number < 99 || number > 1000)
  {
@@ -10,8 +10,62 @@ while (number < 99 || number > 1000)
  if (number >= 100 && number <= 999)
  {
 		Console.WriteLine(number % 10);
- }
+ } */
 
 // Задача 13: Напишите программу, которая выводит третью цифру
 // заданного числа или сообщает, что третьей цифры нет.
 // НЕ ИСПОЛЬЗОВАТЬ НУМЕРАЦИЮ СТРОК
+
+
+// для себя решение оставил ,а так я сократил несколько строк кода не нужных ))
+
+/* Console.WriteLine("Введите любое число");
+int number = int.Parse(Console.ReadLine());
+int numberA = number;
+
+if (number < 99)
+    {
+        Console.Write($" В числе {number} третьей цифры нет");
+		  Console.WriteLine("Введите любое число: ");
+		  number = int.Parse(Console.ReadLine());
+    }
+    else
+    {
+         if (number >= 100 && number <= 999)
+			{
+		  	Console.WriteLine($"Третья цифра в числе {number} , будет {number % 10}");
+			}
+			else
+			{
+				while (number > 999)
+					number = number / 10;
+					Console.WriteLine($"Третья цифра в числе {numberA} , будет {number % 10}");
+			}
+    }
+ */
+ 
+// Вводим число больше 999 , то с помощью цикла делим на 10 до тех пор , 
+// пока не будет число меньше 999, а после выводим остаток с помощью оператора %
+
+Console.WriteLine("Введите любое число");
+int number = int.Parse(Console.ReadLine());
+int numberA = number;
+
+while (number < 99)
+    {
+        Console.WriteLine($"В числе {number} третьей цифры нет");
+		  Console.WriteLine("Введите любое число: ");
+		  number = int.Parse(Console.ReadLine());
+    }
+    {
+         if (number >= 100 && number <= 999)
+			{
+		  	Console.WriteLine($"Третья цифра в числе {number} , будет {number % 10}");
+			}
+			else
+			{
+				while (number > 999)
+					number = number / 10;
+					Console.WriteLine($"Третья цифра в числе {numberA} , будет {number % 10}");
+			}
+    }
