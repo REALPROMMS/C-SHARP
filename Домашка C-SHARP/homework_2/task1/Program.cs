b38f139c2e8f7d4e1991b0b06a2b5ddf258606b8
REALPROMMS <m.makarov90@bk.ru>
2022-12-07 16:52:39 +0300

    done task1

--- Домашка C-SHARP/homework_2/task1/Program.cs
+++ Домашка C-SHARP/homework_2/task1/Program.cs
@@ -1,9 +1,23 @@
-﻿Console.Clear();
+﻿/* Console.Clear();
 int numberA = new Random().Next(100, 999);
 
 string n = numberA.ToString();
 Console.WriteLine(numberA);
-Console.WriteLine(n[1]);	
+Console.WriteLine(n[1]); */	
+
+
 
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное
 // число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+int numberA = new Random().Next(100, 999);
+Console.WriteLine(numberA);
+
+numberA = numberA / 10;
+numberA = numberA % 10;
+
+Console.WriteLine(numberA);
+
+// Решил задачу без использования метода ToString().
+
