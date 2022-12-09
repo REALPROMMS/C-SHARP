16bd44068f9da1617b29e5af58749ae367ee8a68
REALPROMMS <m.makarov90@bk.ru>
2022-12-09 10:58:55 +0300

    done task5

---
+++ Практика C-SHARP/seminars4/task5/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+ */
+
+ // task 24
+Console.Clear();
+Console.Write("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((1 + n) / 2 * n);
