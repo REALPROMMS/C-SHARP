4a9cb360c7c6d5792954b5aaea6b86ea664a3522
REALPROMMS <m.makarov90@bk.ru>
2022-12-09 09:19:27 +0300

    done task3

---
+++ Практика C-SHARP/seminars3/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координату X: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1 || n == 2)
+Console.WriteLine("y > 0");
+else
+Console.WriteLine("y < 0");
+
+if (n == 1 || n == 4)
+Console.WriteLine("x > 0");
+else
+Console.WriteLine("x < 0");
+
+//Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
