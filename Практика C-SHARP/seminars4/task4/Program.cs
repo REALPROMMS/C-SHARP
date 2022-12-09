0c49497716f90ddaa8a2ddd3b76d38e6ca35fb05
REALPROMMS <m.makarov90@bk.ru>
2022-12-09 10:58:45 +0300

    done task4

---
+++ Практика C-SHARP/seminars4/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿void test(ref int a)
+{
+    a += 5;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+test(ref n);
+Console.WriteLine(n);
+
+// ref - передача ссылки на этот элемент
