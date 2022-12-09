f91b2a437bb5a2cc65b387c34c7580d764b51fdb
REALPROMMS <m.makarov90@bk.ru>
2022-12-09 10:12:47 +0300

    done task4

--- Домашка C-SHARP/homework_2/task4(dop)/Program.cs
+++ Домашка C-SHARP/homework_2/task4(dop)/Program.cs
@@ -1,1 +1,17 @@
-﻿/* не смог решить :( */
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), max1 = n, max2 = -1;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (max1 < n)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if (max2 < n)
+    {
+        max2 = n;
+    }
+}
+Console.WriteLine(max2);
