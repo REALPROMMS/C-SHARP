66c505a4ae93b605d9021201ba0aab56f4ff9277
REALPROMMS <m.makarov90@bk.ru>
2022-12-09 11:09:56 +0300

    done task7

---
+++ Практика C-SHARP/seminars4/task7/Program.cs
@@ -0,0 +1,9 @@
+﻿ // task 27
+
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+for (int i = 1; i <= n; i++)
+	sum = sum * i;
+Console.WriteLine(sum);
