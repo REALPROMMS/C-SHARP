2ed55ab359ff76d953ac22ea7848d6d2df25b401
REALPROMMS <m.makarov90@bk.ru>
2022-12-11 12:02:47 +0300

    done task4

---
+++ Практика C-SHARP/seminars3/task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// task 21
+
+Console.Clear();
+Console.Write("Введите X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine(Math.Round(dist, 2));
+
+// Math.Round - отвечает за округление (число котрое округляешм, сколько знаков после запятой)
+// Если не указать сколько знаков , то будет округлять до целого числа.
