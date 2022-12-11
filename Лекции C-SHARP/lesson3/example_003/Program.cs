2b3b1e72698fc207fc39ab88b45f31f4f13729c3
REALPROMMS <m.makarov90@bk.ru>
2022-12-11 11:19:49 +0300

    done 003

---
+++ Лекции C-SHARP/lesson3/example_003/Program.cs
@@ -0,0 +1,38 @@
+﻿// =====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить болоьшими "К"
+// а в большие "С" заменить маленькими "с"
+
+
+string text = "-Я думаю, - сказал князь, улыбаясь, -что,"
+					+ "ежели бы вас послали вместо нашего милого";
+// string s = "qwerty"
+//             0123
+// s[3]  // r
+
+string Replace(string text, char oldValue, chaer newValue)
+{
+	string result = String.Empty;
+
+	int length = text.Length;
+	for (int i = 0; i < length; i++)
+	{
+		if(text[i] == oldValue) 
+		result = result + $"{newValue}";
+		else result = result + $"{text[i]}";
+	}
+		return;
+}
+
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+string newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+string newText = Replace(text, 'С', 'с');
+Console.WriteLine(newText);
+Console.WriteLine();
