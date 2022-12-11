76932eb128074defbb885d2c29e377e02b7b3188
REALPROMMS <m.makarov90@bk.ru>
2022-12-11 15:39:14 +0300

    done task1

--- Домашка C-SHARP/homework_4/task1/Program.cs
+++ Домашка C-SHARP/homework_4/task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать функцию Math.Pow
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите степень в которую нужно возвести число {number} : ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int numberDeg = 1;
+
+	for (int i = 1; i <= degree; i++)
+	{
+		numberDeg = numberDeg * number;
+	}
+	
+Console.WriteLine(numberDeg);
