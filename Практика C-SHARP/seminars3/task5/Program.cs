c49d00b7b8363224cb54accfc66798a5de93a5b5
REALPROMMS <m.makarov90@bk.ru>
2022-12-11 12:25:32 +0300

    done task5

---
+++ Практика C-SHARP/seminars3/task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+double sum = 0;
+for ( i = 1; i <= n; i++)
+	Console.WriteLine(sum = Math.Pow(i, 2));
+
+
+// Вывод число от 1 до N в квадрат
