5f958abcbc62fa2a60868857043710e7077691ed
REALPROMMS <m.makarov90@bk.ru>
2022-12-13 11:55:11 +0300

    done task3

--- Домашка C-SHARP/homework_4/task3/Program.cs
+++ Домашка C-SHARP/homework_4/task3/Program.cs
@@ -6,8 +6,8 @@ Console.Write("Введите кол-во элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 Console.WriteLine("Введите массив чисел: ");
-for (int i = 0; i < n; i++)
-{
-array[i] = Convert.ToInt32(Console.ReadLine());
-}
+	for (int i = 0; i < n; i++)
+	{
+	array[i] = Convert.ToInt32(Console.ReadLine());
+	}
 Console.WriteLine($"[{string.Join(", ", array)}]");
