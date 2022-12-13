97a12176f8d4ffd430d31b9f64903f73480e2ed5
REALPROMMS <m.makarov90@bk.ru>
2022-12-13 11:06:13 +0300

    done task5

---
+++ Практика C-SHARP/seminars5/task5/Program.cs
@@ -0,0 +1,52 @@
+﻿// 37 task
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11); // [1, 10]
+}
+
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+        Console.Write($"{array[i] * array[array.Length - 1 - i]} ");
+
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+
+// // 35 task
+
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(1, 11); // [1, 10]
+// }
+
+
+// int[] ReleaseArray(int[] array)
+// {
+//     int[] result = new int[array.Length / 2 + array.Length % 2];
+//     for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+//         result[i] = array[i] * array[array.Length - 1 - i];
+//     return result;
+
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", ReleaseArray(array))}]");
+
