75f2f32ae9cf0e01800977c4be457a7ecfafc1d2
REALPROMMS <m.makarov90@bk.ru>
2022-12-15 13:04:56 +0300

    done 002

---
+++ Лекции C-SHARP/lesson4/example_002/Program.cs
@@ -0,0 +1,15 @@
+﻿double Factorial(int n) // поставили double  потому что в int , после 17! идет уже минусовые числа так как не 		вмещают в себя так много чисел!
+{
+
+	// 1! = 1
+	// 0! = 1
+	if (n == 1)
+		return 1;
+	else  
+		return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+	Console.WriteLine($"{i}! = { Factorial(i)}");
+}
+
