716025b350522c5015ea011cd6b2adc3a52dd2a2
REALPROMMS <m.makarov90@bk.ru>
2022-12-19 11:15:36 +0300

    done task1

--- Практика C-SHARP/seminars6/task1/Program.cs
+++ Практика C-SHARP/seminars6/task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// split показывает через какой символ будет разедление наших элементов
+
+// string[] s = Console.ReadLine().Split("а"); 
+// for (int i = 0; i < s.Length; i++)
+// Console.WriteLine(s[i]);
+
+// Select переберает каждый элемент массива и делает его числом (x - придумываем название сами)
+
+
+// int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// for (int i = 0; i < s.Length; i++)
+// Console.WriteLine(s[i] * 10);
+
+// // forЕach разновидность цикла for (elemnt это тоже самое что i - придумываем название сами)
+
+// int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// forЕach (int element in s)
+// Console.WriteLine(element * 2)
