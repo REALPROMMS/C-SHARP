5178959803829bd4313e9cde3ffd6fb34773771c
REALPROMMS <m.makarov90@bk.ru>
2022-12-19 12:11:05 +0300

    done task5

---
+++ Практика C-SHARP/seminars6/task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Перевод в двоичную систему
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = string.Empty;
+while (n > 0)
+{
+	result = result + Convert.ToString(n % 2); // result - нужно поставить после Convert
+	n /= 2; // n = n / 2;
+}
+Console.WriteLine(result);
