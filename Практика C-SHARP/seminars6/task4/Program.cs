c5bcb2dbf97fe3065f351c4b5c21af0eabd84009
REALPROMMS <m.makarov90@bk.ru>
2022-12-19 12:10:54 +0300

    done task4

---
+++ Практика C-SHARP/seminars6/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверяем треугольник это или нет. По теореме неравенства треугольников.
+
+Console.Clear();
+Console.Write("Введите 3 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a < b + c && b < a + c && c < a + b)
+Console.WriteLine("Треугольник");
+else
+Console.WriteLine("Нет");
