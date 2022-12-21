03d8296256794219757abb1d4c4268586c5c9906
REALPROMMS <m.makarov90@bk.ru>
2022-12-21 11:10:18 +0300

    done task1

--- Практика C-SHARP/seminars7/task1/Program.cs
+++ Практика C-SHARP/seminars7/task1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// double n = new Random().NextDouble() * (end - begin) + begin
+Console.WriteLine(new Random().NextDouble() * (50 - 10) + 10); // [10, 50]
 Console.WriteLine(new Random().NextDouble() * (10 + 10) - 10); // [-10, 10]
+                                             
