5200d834ea605724264adc6e9acf6c1354981801
REALPROMMS <m.makarov90@bk.ru>
2022-12-25 11:23:52 +0300

    done

--- Домашка C-SHARP/homework_5/task1/Program.cs
+++ Домашка C-SHARP/homework_5/task1/Program.cs
@@ -7,7 +7,7 @@ for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(100, 1000);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 
-	int numberEven = 0;
+int numberEven = 0;
 for (int j = 0; j < array.Length; j++)
 {
 	if (array[j] % 2 != 0);
