d1628be61908786cd31ea8d3986acccb3fa5da7f
REALPROMMS <m.makarov90@bk.ru>
2022-12-30 03:42:33 +0300

    done task7

---
+++ Практика C-SHARP/seminars9/task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 69. Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+int rec(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return rec(a, b - 1) * a;
+
+}
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
