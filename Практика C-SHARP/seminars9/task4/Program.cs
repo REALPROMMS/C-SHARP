f6a6dd0b1fdbbe28e4d5ed179a81c5c51af02e8b
REALPROMMS <m.makarov90@bk.ru>
2022-12-30 03:42:05 +0300

    done task4

---
+++ Практика C-SHARP/seminars9/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 63. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+string rec(int n)
+{
+    if (n == 1)
+        return "1 ";
+    return $"{n} " + rec(n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
