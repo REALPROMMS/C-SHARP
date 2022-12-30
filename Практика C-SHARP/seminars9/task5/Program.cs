4378991c973805198f777e3853e5cb291eecc1a7
REALPROMMS <m.makarov90@bk.ru>
2022-12-30 03:42:14 +0300

    done task5

---
+++ Практика C-SHARP/seminars9/task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string rec(int m, int n)
+{
+    if (n == m)
+        return $"{n} ";
+    return rec(m, n - 1) + $"{n} ";
+}
+
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
